291c22956ace8ec8607cacefe4f3a31fab6568cc
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-05-13 15:40:38 -0400

    1988_KR_C_PL: C1 -- ex 1-17, p. 30!

--- C1_A_Tutorial_Introduction/1_9_Character_Arrays/prax_exercises/ex1_17/src/ex1_17.c
+++ C1_A_Tutorial_Introduction/1_9_Character_Arrays/prax_exercises/ex1_17/src/ex1_17.c
@@ -18,15 +18,16 @@ int main()
     char longest[MAXLINE];      /* longest line saved here */
 
     max = 0;
-    while ((len = getline(line, MAXLINE)) > 0) {
-        if (len > OUTLEN) {
+    while ((len = getline(line, MAXLINE)) > 0)
+        if (len > max) {
             max = len;
             copy(longest, line);
-            printf("The following line is %d characters long.", max);
-            printf("\n");
-            printf("The line is: %s", longest);         
         }
-    }   
+    if (max > 0)    /* there was a line */
+        printf("The longest line is %d characters long.", max);
+        printf("\n");
+        printf("The line is: %s", longest);
+        
     return 0;
 };
 
