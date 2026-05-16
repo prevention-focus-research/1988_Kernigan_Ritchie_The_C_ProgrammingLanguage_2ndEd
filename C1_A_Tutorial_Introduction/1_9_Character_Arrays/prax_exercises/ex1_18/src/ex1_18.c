5d911517c58e40cd741ed50cb554b693ab619d73
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-05-15 22:13:12 -0400

    1988_KR_C_PL: C1 -- ex 1-18, p. 30!

---
+++ C1_A_Tutorial_Introduction/1_9_Character_Arrays/prax_exercises/ex1_18/src/ex1_18.c
@@ -0,0 +1,39 @@
+
+
+/* Exercise 1-16: Write a program to remove trailing blanks and tabs from each line of input, 
+and to delete entirely blank lines. */
+
+#include <stdio.h>
+
+#define MAXLINE 1000    /* Maximum input line length */
+#define LIMIT 80        /* Threshold for printing */
+
+int getline(char line[], int maxline);
+
+int main() {
+    int len;                /* current line length */
+    char line[MAXLINE];     /* current input line */
+
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        if (len > LIMIT) {
+            printf("%s", line);
+        }
+    }
+
+    return 0;
+}
+
+/* Function to read a line into s, return length */
+int get_line(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
