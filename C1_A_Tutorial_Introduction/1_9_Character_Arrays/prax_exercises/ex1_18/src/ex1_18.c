a22062990cf08feffecb3e52353042679e6c0c17
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-05-19 18:08:43 -0400

    1988_KR_C_PL: C1 -- ex 1-18, p. 30!

--- C1_A_Tutorial_Introduction/1_9_Character_Arrays/prax_exercises/ex1_18/src/ex1_18.c
+++ C1_A_Tutorial_Introduction/1_9_Character_Arrays/prax_exercises/ex1_18/src/ex1_18.c
@@ -1,6 +1,6 @@
 
 
-/* Exercise 1-16: Write a program to remove trailing blanks and tabs from each line of input, 
+/* Exercise 1-18: Write a program to remove trailing blanks and tabs from each line of input, 
 and to delete entirely blank lines. */
 
 #include <stdio.h>
