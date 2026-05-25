dd1470dc888d97ba93cc04cf286f6326811ee3f1
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-05-25 19:12:58 -0400

    1988_KR_C_PL: C1 -- ex 1-18, p. 30!

---
+++ C1_A_Tutorial_Introduction/1_9_Character_Arrays/prax_exercises/ex1_18a/src/ex1_18a_rm_tb_ttabs.c
@@ -0,0 +1,14 @@
+/* 1. remove trailing blanks from each line of input.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' || c != '\t') {
+            
+        }
+    }
+}
