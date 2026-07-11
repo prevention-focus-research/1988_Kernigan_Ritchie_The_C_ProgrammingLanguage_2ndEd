f82cc57f05a249b2c5a673788c048d6df7d72bcd
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-07-11 13:48:29 -0400

    1988_KR_C_PL: C1 -- ex 1-20, p. 34!

--- C1_A_Tutorial_Introduction/1_10_ExternalVariables_and_Scope/prax_exercises/ex1_20/src/detab.c
+++ C1_A_Tutorial_Introduction/1_10_ExternalVariables_and_Scope/prax_exercises/ex1_20/src/detab.c
@@ -5,7 +5,25 @@ a symbolic parameter? */
 
 #include <stdio.h>
 
+#define MAXLINE 1000;
+
+int my_getline(char line[], int max);
+
 int main() {
     
     return 0;
+}
+
+int my_getline(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
