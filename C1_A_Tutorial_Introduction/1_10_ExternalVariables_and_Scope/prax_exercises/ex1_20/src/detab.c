8487e19eebc8e3efb7e4ea47f13b5ad8efa6ba5d
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-08-31 10:23:02 -0400

    1988_KR_C_PL: C1 -- ex 1-20, p. 34!

--- C1_A_Tutorial_Introduction/1_10_ExternalVariables_and_Scope/prax_exercises/ex1_20/src/detab.c
+++ C1_A_Tutorial_Introduction/1_10_ExternalVariables_and_Scope/prax_exercises/ex1_20/src/detab.c
@@ -43,13 +43,13 @@ int main() {
                 next_tab_pos = num_tab_stops_crossed*n + n;
             }
             if (line[i] == '\t') {
-                line_cursor = i;
+                tab_cursor = i;
                 num_spaces_to_add = next_tab_pos - line_cursor;
                 for (space_cursor = 0; space_cursor < num_spaces_to_add; ++space_cursor) {
-                    tabline[line_cursor + space_cursor] = ' ';
+                    tabline[tab_cursor + space_cursor] = ' ';
                 }
                 ++i;
-                tab_cursor=i;
+                tab_cursor=tab_cursor + space_cursor;
             } else {
                 tabline[tab_cursor] = line[i];
             }
