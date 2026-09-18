006273ea262b62e18e251d648b01ac32782f208c
prevention-focus-research <209988406+prevention-focus-research@users.noreply.github.com>
2026-09-18 18:03:31 -0400

    1988_KR_C_PL: C1 -- ex 1-20, p. 34!

--- C1_A_Tutorial_Introduction/1_10_ExternalVariables_and_Scope/prax_exercises/ex1_20/src/detab.c
+++ C1_A_Tutorial_Introduction/1_10_ExternalVariables_and_Scope/prax_exercises/ex1_20/src/detab.c
@@ -106,14 +106,14 @@ int my_getline(char s[], int lim)
 
 void copy_detab(char to[], char from[], int to_len) {
     
-    int i, currentpos, ntabs_chars=0, nexttabpos=0;
+    int i, ntab_chars=0;
     int line_cursor, num_spaces_to_add, space_cursor, tab_cursor, next_tab_pos, num_tab_stops_crossed=0, n=4;
 
     next_tab_pos = num_tab_stops_crossed*n + n;
 
     for (i = 0; i < (to_len - 1); ++i) {
         if (to[i] == '\t') {
-            
+            ++ntab_chars;
         }
         from[i] = to[i];
     }
